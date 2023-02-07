07f3eb7d1e74f5b42ad6ae6d740b07a562639ef0
rayanramazan <rayanramazan09@gmail.com>
2023-02-08 00:59:32 +0300

    Initial commit

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container">
+  <h1 class="text-primary text-4xl"> HELLO </h1>
+  </div>
+</template>
+
+<script setup>
+
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    runtimeConfig: {
+        // The private keys which are only available server-side
+        apiSecret: '123',
+        // Keys within public are also exposed client-side
+        public: {
+          apiBase: '/api'
+        }
+      },
+      modules: [
+          'nuxt-windicss',
+          'nuxt-headlessui'
+      ],
+      
+})
