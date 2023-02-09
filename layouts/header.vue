f4d6586491f80c03f334b83645c31251a0100d32
rayanramazan <rayanramazan09@gmail.com>
2023-02-09 21:12:14 +0300

    add sidebar

--- layouts/header.vue
+++ layouts/header.vue
@@ -1,37 +1,42 @@
 <template>
   <div class="w-full gap-4 px-4 pt-4 items-start flex flex-row-reverse">
-  <div class="w-full py-4 flex justify-between lg:justify-end items-center px-4 bg-white/5 rounded-md border-1 border-white/10">
-    <button class="hidden">
-      <Icon name="gg:menu-left" class="w-7 h-7 text-white"/>
-    </button>
-    <div class="flex items-center gap-3 text-white">
-      <NuxtLink to="">
-        <Icon
-            class="w-6 h-6 transform hover:-translate-y-1 cursor-pointer duration-300"
-            name="ion:logo-instagram" />
-      </NuxtLink>
-      <NuxtLink to="">
-        <Icon
-            class="w-6 h-6 transform hover:-translate-y-1 cursor-pointer duration-300"
-            name="ion:social-facebook" />
-      </NuxtLink>
-      <NuxtLink to="">
-        <Icon
-            class="w-6 h-6 transform hover:-translate-y-1 cursor-pointer duration-300"
-            name="ion:logo-github" />
-      </NuxtLink>
-      <NuxtLink to="">
-        <Icon
-            class="w-6 h-6 transform hover:-translate-y-1 cursor-pointer duration-300"
-            name="ion:logo-twitter" />
-      </NuxtLink>
-      <NuxtLink to="">
-        <Icon
-            class="w-6 h-6 transform hover:-translate-y-1 cursor-pointer duration-300"
-            name="ion:logo-linkedin" />
-      </NuxtLink>
+
+    <div class="w-full flex flex-col">
+      <div class="py-4 flex justify-between lg:justify-end items-center px-4 bg-white/5 rounded-md border-1 border-white/10">
+        <button class="hidden">
+          <Icon name="gg:menu-left" class="w-7 h-7 text-white"/>
+        </button>
+        <div class="flex items-center gap-3 text-white">
+          <NuxtLink to="">
+            <Icon
+                class="w-6 h-6 transform hover:-translate-y-1 cursor-pointer duration-300"
+                name="ion:logo-instagram" />
+          </NuxtLink>
+          <NuxtLink to="">
+            <Icon
+                class="w-6 h-6 transform hover:-translate-y-1 cursor-pointer duration-300"
+                name="ion:social-facebook" />
+          </NuxtLink>
+          <NuxtLink to="">
+            <Icon
+                class="w-6 h-6 transform hover:-translate-y-1 cursor-pointer duration-300"
+                name="ion:logo-github" />
+          </NuxtLink>
+          <NuxtLink to="">
+            <Icon
+                class="w-6 h-6 transform hover:-translate-y-1 cursor-pointer duration-300"
+                name="ion:logo-twitter" />
+          </NuxtLink>
+          <NuxtLink to="">
+            <Icon
+                class="w-6 h-6 transform hover:-translate-y-1 cursor-pointer duration-300"
+                name="ion:logo-linkedin" />
+          </NuxtLink>
+        </div>
+
+      </div>
+      <slot />
     </div>
-  </div>
 
     <div class="w-[20%] p-4 bg-white/5 rounded-md border-1 border-white/10">
       <div class="flex justify-center items-center gap-1 text-3xl font-bold">
@@ -48,7 +53,7 @@
 
   </div>
 
-  <slot />
+
   <NuxtLayout name="footer" />
 </template>
 <script setup lang="ts">
--- layouts/sidebar.vue
+++ layouts/sidebar.vue
@@ -1,54 +1,97 @@
 <template>
-  <ul class=" flex flex-col">
-    <li>
+  <ul class=" flex flex-col gap-2 text-white">
+    <li class="text-primary font-bold">
       <NuxtLink href="">
         Home
       </NuxtLink>
     </li>
     <li>
       <NuxtLink href="">
-        Home
+        Components
       </NuxtLink>
     </li>
     <li>
       <NuxtLink href="">
-        Home
+        Resources
       </NuxtLink>
     </li>
     <li>
       <NuxtLink href="">
-        Home
+        Contact Us
       </NuxtLink>
     </li>
     <li>
-      <NuxtLink href="">
-        Home
+      <hr class="my-2 border-white/10">
+      <span class="font-bold">
+        Tools
+      </span>
+    </li>
+    <li>
+      <NuxtLink href="" class="ml-2">
+        Border
       </NuxtLink>
     </li>
     <li>
-      <NuxtLink href="">
-        Home
+      <NuxtLink href="" class="ml-2">
+        Box Shadow
       </NuxtLink>
     </li>
     <li>
-      <NuxtLink href="">
-        Home
-      </NuxtLink>
-    </li>
-    <li>
-      <a href="">
-        Shape
+      <a href="" class="ml-2">
+        Border Radius
       </a>
     </li>
     <li>
-      <a href="">
-        Rounded
+      <a href="" class="ml-2">
+        RGB Colors
       </a>
     </li>
     <li>
-      <a href="">
-        Component
-      </a>
+      <hr class="my-2 border-white/10">
+      <Menu
+          as="div"
+          class="static inline-block">
+        <MenuButton
+            class="rounded-md py-2"
+            @click="isOpen = true"
+        >
+
+          <div
+              class="flex justify-between items-center">
+        <span>
+        Borders
+      </span>
+            <Icon name="ion:md-arrow-dropdown" class="w-5 h-5" />
+          </div>
+        </MenuButton>
+
+        <transition
+            enter-active-class="transition duration-100 ease-out"
+            enter-from-class="transform scale-95 opacity-0"
+            enter-to-class="transform scale-100 opacity-100"
+            leave-active-class="transition duration-75 ease-in"
+            leave-from-class="transform scale-100 opacity-100"
+            leave-to-class="transform scale-95 opacity-0"
+        >
+          <MenuItems
+              class="mt-1 ml-2 text-white"
+              v-show="isOpen">
+
+            <MenuItem class="flex flex-col text-dark lg:text-light">
+              <span>
+                hello
+              </span>
+            </MenuItem>
+
+
+          </MenuItems>
+        </transition>
+      </Menu>
     </li>
   </ul>
 </template>
+
+<script setup lang="ts">
+import {Menu, MenuButton, MenuItem, MenuItems} from "@headlessui/vue";
+const isOpen = ref(false)
+</script>
