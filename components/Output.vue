70b8abd14a201906dfbd74b0149e6ef49feca7ec
rayanramazan <rayanramazan09@gmail.com>
2023-02-18 17:23:26 +0300

    button copy code

--- components/Output.vue
+++ components/Output.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="w-full flex justify-center gap-4 flex-col">
       <div class="text-center flex flex-col hover:bg-primary/5 duration-300 bg-primary/10 border-1 border-primary p-6 rounded text-lg text-white">
-        <span>
+        <span ref="textToCopy">
           {{ props.stylesheet }}
           {{ props.data }};
         </span>
       </div>
-    <button class="bg-primary text-white p-2 rounded">
-      Copy
+    <button
+        @click="copyText"
+        class="bg-primary text-white p-2 rounded">
+      <span v-if="response === true">
+        Copied
+      </span>
+      <span v-else>
+        Copy
+      </span>
     </button>
   </div>
+
 </template>
 
 <script setup>
@@ -18,4 +26,22 @@ const props = defineProps({
   stylesheet: String
 })
 
+const textToCopy = ref(null)
+const response = ref(false)
+// Function to copy the text to the clipboard
+const copyText = () => {
+  if (textToCopy.value) {
+    const text = textToCopy.value.innerText
+    navigator.clipboard.writeText(text)
+    response.value = true
+  }
+}
+
+watch(response, (newValue) => {
+  if (newValue) {
+    setTimeout(() => {
+      response.value = false
+    }, 3000)
+  }
+})
 </script>
