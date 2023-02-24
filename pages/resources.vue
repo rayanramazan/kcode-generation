87fcd3e1224251adce93b6a049e1721e53e15655
rayanramazan <rayanramazan09@gmail.com>
2023-02-24 20:14:36 +0300

    slug page resources and update head

--- pages/resources.vue
+++ pages/resources.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="w-full flex gap-4 my-8 justify-center flex-wrap">
+  <div class="m-8 text-center">
+    <h1 class="text-2xl font-bold text-white">KTOOLS</h1>
+    <span class="text-white/70 ">
+    We’ve made a few other resources to help you with your design and development workflow.
+  </span>
+  </div>
+  <div class="w-full flex gap-4 justify-center flex-wrap">
     <NuxtLink
     v-for="resource in resources"
     :key="resource"
