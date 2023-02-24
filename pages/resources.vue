fea82f6fae15d60c5a8113a5a4673058ef5fc550
rayanramazan <rayanramazan09@gmail.com>
2023-02-24 20:09:47 +0300

    fetch resources

--- pages/resources.vue
+++ pages/resources.vue
@@ -1,64 +1,19 @@
 <template>
   <div class="w-full flex gap-4 my-8 justify-center flex-wrap">
-    <NuxtLink>
+    <NuxtLink
+    v-for="resource in resources"
+    :key="resource"
+    :to="`resource/${resource.slug}`"
+    >
       <img
-          class="w-full h-60 object-cover rounded-lg sm:h-52 filter hover:grayscale-50 transform hover:-translate-y-1 duration-300"
-          loading="lazy"
-          alt="img"
-          src="http://drive.google.com/uc?export=view&id=1Og6F8rljK2zyhTA28_Syf0PQOq3XJ6ZO" />
+          class="w-full sm:w-full h-full object-cover rounded-lg sm:h-52 filter hover:grayscale-50 transform hover:-translate-y-1 duration-300"
+          :alt="resource.id"
+          :srcset="`http://drive.google.com/uc?export=view&id=${resource.img}`"
+          :src="`http://drive.google.com/uc?export=view&id=${resource.img}`" />
     </NuxtLink>
-    <NuxtLink>
-      <img
-          class="w-full h-60 object-cover rounded-lg sm:h-52"
-          loading="lazy"
-          alt="img"
-          src="http://drive.google.com/uc?export=view&id=1Og6F8rljK2zyhTA28_Syf0PQOq3XJ6ZO" />
-    </NuxtLink>
-    <NuxtLink>
-      <img
-          class="w-full h-60 object-cover rounded-lg sm:h-52"
-          loading="lazy"
-          alt="img"
-          src="http://drive.google.com/uc?export=view&id=1Og6F8rljK2zyhTA28_Syf0PQOq3XJ6ZO" />
-    </NuxtLink>
-    <NuxtLink>
-      <img
-          class="w-full h-60 object-cover rounded-lg sm:h-52"
-          loading="lazy"
-          alt="img"
-          src="http://drive.google.com/uc?export=view&id=1Og6F8rljK2zyhTA28_Syf0PQOq3XJ6ZO" />
-    </NuxtLink>
-    <NuxtLink>
-      <img
-          class="w-full h-60 object-cover rounded-lg sm:h-52"
-          loading="lazy"
-          alt="img"
-          src="http://drive.google.com/uc?export=view&id=1Og6F8rljK2zyhTA28_Syf0PQOq3XJ6ZO" />
-    </NuxtLink>
-    <NuxtLink>
-      <img
-          class="w-full h-60 object-cover rounded-lg sm:h-52"
-          loading="lazy"
-          alt="img"
-          src="http://drive.google.com/uc?export=view&id=1Og6F8rljK2zyhTA28_Syf0PQOq3XJ6ZO" />
-    </NuxtLink>
-    <NuxtLink>
-      <img
-          class="w-full h-60 object-cover rounded-lg sm:h-52"
-          loading="lazy"
-          alt="img"
-          src="http://drive.google.com/uc?export=view&id=1Og6F8rljK2zyhTA28_Syf0PQOq3XJ6ZO" />
-    </NuxtLink>
-    <NuxtLink>
-      <img
-          class="w-full h-60 object-cover rounded-lg sm:h-52"
-          loading="lazy"
-          alt="img"
-          src="http://drive.google.com/uc?export=view&id=1Og6F8rljK2zyhTA28_Syf0PQOq3XJ6ZO" />
-    </NuxtLink>
+
   </div>
 </template>
 <script setup lang="ts">
-
-
+const resources = await useResources()
 </script>
