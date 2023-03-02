2c0ded00aa3e7a930efa7c55be4e3351cca6c9fb
rayanramazan <rayanramazan09@gmail.com>
2023-03-02 22:45:54 +0300

    animation delay item

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="py-6 px-4 sm:p-6 md:py-10 md:px-8">
+  <main class="py-6 px-4 sm:p-6 md:py-10 md:px-8 animate-jackInTheBox">
     <div class="max-w-4xl mx-auto grid grid-cols-1 lg:max-w-5xl lg:gap-x-20 lg:grid-cols-2">
       <div class="relative p-3 col-start-1 row-start-1 flex flex-col-reverse rounded-lg bg-gradient-to-t from-black/75 via-black/0 sm:bg-none sm:row-start-2 sm:p-0 lg:row-start-1">
         <h1 class="mt-1 text-lg font-semibold text-white sm:text-slate-900 md:text-2xl dark:sm:text-white">Beach House in Collingwood</h1>
--- windi.config.ts
+++ windi.config.ts
@@ -117,5 +117,15 @@ export default defineConfig({
     TypographyPlugin as Plugin,
     AspectRatioPlugin as Plugin,
     require('@windicss/plugin-scrollbar'),
+    require('@windicss/plugin-animations')({
+      settings: {
+        animatedSpeed: 1000,
+        heartBeatSpeed: 1000,
+        hingeSpeed: 2000,
+        bounceInSpeed: 750,
+        bounceOutSpeed: 750,
+        animationDelaySpeed: 1000,
+      },
+    }),
   ] as Plugin[],
 })
