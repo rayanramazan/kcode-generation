a216aef4ab4a417c7c48cafb8723583f43b48156
rayanramazan <rayanramazan09@gmail.com>
2023-03-22 02:15:31 +0300

    transition delay

--- components/transitions/transition-delay.vue
+++ components/transitions/transition-delay.vue
@@ -1,9 +1,9 @@
 <template>
     <span class="mt-8 mb-4 text-2xl text-white font-bold">
-        Transitions
+        Transitions Delay
     </span>
         <div class="w-full px-2 sm:px-0">
-      <TabGroup>
+      <TabGroup  >
         <TabList class="flex space-x-1 rounded-xl bg-primary/20 p-1">
           <Tab
             as="template"
@@ -38,6 +38,7 @@
             </button>
           </Tab>
           <Tab
+          style="z-index: 999;"
             as="template"
             v-slot="{ selected }"
           >
@@ -109,47 +110,36 @@ import { VAceEditor } from 'vue3-ace-editor';
   import { TabGroup, TabList, Tab, TabPanels, TabPanel } from '@headlessui/vue'
 
   const transition_timing_function = ref({
-        html_code: '<div class="square ttf-linear bg-primary" tabindex="0">linear</div>\n'+
-                    '<div class="square ttf-ease bg-primary" tabindex="0">ease</div>\n'+
-                    '<div class="square ttf-ease-in bg-primary" tabindex="0">ease-in</div>\n'+
-                    '<div class="square ttf-ease-out bg-primary" tabindex="0">ease-out</div>\n'+
-                    '<div class="square ttf-ease-in-out bg-primary" tabindex="0">ease-in-out</div>\n'+
-                    '<div class="square ttf-steps bg-primary" tabindex="0">steps</div>\n'+
-                    '<div class="square ttf-cubic bg-primary" tabindex="0">cubic</div>\n',
-        css_code: '.square {\n'+
-                    '   width: 150px;\n'+
-                    '   max-height: 100px;\n'+
-                    '   background-color: lightcoral;\n'+
-                    '   border-radius: 8px;\n'+
-                    '   text-align: center;\n'+
-                    '   padding: 8px;\n'+
-                    '   margin-bottom: 4px;\n'+
-                    '   transition-property: width;\n'+
-                    '   transition-duration: 1s;\n'+
+        html_code: '<div class="rectangle bg-primary" tabindex="0">no delay</div>\n'+
+                    '<div class="rectangle bg-primary" tabindex="0">100ms delay</div>\n'+
+                    '<div class="rectangle bg-primary" tabindex="0">200ms delay</div>\n'+
+                    '<div class="rectangle bg-primary" tabindex="0">500ms delay</div>\n'+
+                    '<div class="rectangle bg-primary" tabindex="0">1000ms delay</div>\n',
+        css_code: '.rectangle {\n' +
+                    '   width: 150px;\n' +
+                    '   max-height: 100px;\n' +
+                    '   border-radius: 8px;\n' +
+                    '   text-align: right;\n' +
+                    '   padding: 8px;\n' +
+                    '   margin-bottom: 4px;\n' +
                     '}\n\n'+
-                    '.square.ttf-linear {\n'+
-                    '   transition-timing-function: linear;\n'+
-                    '}\n\n'+
-                    '.square.ttf-ease {\n'+
-                    '   transition-timing-function: ease;\n'+
-                    '}\n\n'+
-                    '.square.ttf-ease-in {\n'+
-                    '   transition-timing-function: ease-in;\n'+
-                    '}\n'+
-                    '.square.ttf-ease-out {\n'+
-                    '   transition-timing-function: ease-out;\n'+
-                    '}\n\n'+
-                    '.square.ttf-ease-in-out {\n'+
-                    '   transition-timing-function: ease-in-out;\n'+
-                    '}\n\n'+
-                    '.square.ttf-steps {\n'+
-                    '   transition-timing-function: steps(3);\n'+
-                    '}\n\n'+
-                    '.square.ttf-cubic {\n'+
-                    '   transition-timing-function: cubic-bezier(0.68, -0.55, 0.27, 1.55);\n'+
-                    '}\n\n'+
-                    '.square:is(:hover, :focus) {\n'+
-                    '   width: 50%;\n'+
+                    '.rectangle:nth-child(1) {\n' +
+                    '   transition-delay: 0s;\n' +
+                    '}\n\n' +
+                    '.rectangle:nth-child(2) {\n' +
+                    '   transition-delay: 100ms;\n' +
+                    '}\n\n' +
+                    '.rectangle:nth-child(3) {\n' +
+                    '   transition-delay: 200ms;\n' +
+                    '}\n\n' +
+                    '.rectangle:nth-child(4) {\n' +
+                    '   transition-delay: 500ms;\n' +
+                    '}\n\n' +
+                    '.rectangle:nth-child(5) {\n' +
+                    '   transition-delay: 1000ms;\n' +
+                    '}\n\n' +
+                    '.rectangle:is(:hover, :focus) {\n' +
+                    '   background-color: transparent;\n' +
                     '}\n'
   })
 </script>
--- pages/transitions.vue
+++ pages/transitions.vue
@@ -2,12 +2,15 @@
 
 
 
-<transition-timing-function />
+<transitionTimingFunction />
+
+<transitionDelay />
 
   </template>
 
   
   <script setup>
   import transitionTimingFunction from '../components/transitions/transition-timing-function'
+  import transitionDelay from '~~/components/transitions/transition-delay.vue';
   </script>
   
