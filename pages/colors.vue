f511b5846e69b5985e414980971f0d7bec529c62
rayanramazan <rayanramazan09@gmail.com>
2023-03-21 03:15:51 +0300

    fetch button and set title

--- pages/colors.vue
+++ pages/colors.vue
@@ -1,10 +1,13 @@
 <template>
 
+<span class="mt-8 text-2xl text-white font-bold">
+  Color Swatches
+</span>
 <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-x-2 gap-y-3 mt-3">
 
   <div 
     v-for="color in colors" :key="color.id"
-    class="flex rounded relative shadow-lg min-h-[200px]">
+    class="flex rounded relative shadow-lg min-h-[160px]">
     <div 
       :style="`background: ${color.primary}`"
       class="w-[50%] h-full"></div>
@@ -21,6 +24,14 @@
     </div>
   </div>
 
+</div>
+
+<div class="grid md:grid-cols-2 lg:grid-cols-3 gap-x-2 gap-y-3 mt-3">
+  <button 
+    :style="`background: ${color.primary};`"
+    v-bind:class="`hover:bg-${color.secondary}`"
+    class="p-2 rounded text-white"
+    v-for="color in colors" :key="color.id">Button</button>
 </div>
 
 </template>
