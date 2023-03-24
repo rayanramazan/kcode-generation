1a67f34d12de50d37bfcabf5e4b20cf768de6d4f
rayanramazan <rayanramazan09@gmail.com>
2023-03-24 21:43:49 +0300

    data file json gradient

---
+++ composables/useGradients.ts
@@ -0,0 +1,9 @@
+import dataGradients from './dataGradients.json'
+type Gradients ={
+    id: number;
+    fisrt_color: string;
+    second_color: string
+}
+
+export default async () =>
+    dataGradients
