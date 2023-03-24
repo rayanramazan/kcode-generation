91186d84e34db04caa6623353b11e890a7042a66
rayanramazan <rayanramazan09@gmail.com>
2023-03-24 19:56:48 +0300

    fetch text with control paragraph and char number

--- pages/text.vue
+++ pages/text.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="flex justify-center items-center gap-8 sm:gap-20 flex-wrap mt-8 mb-4 w-full" style="z-index: 999;">
-        <div class="flex flex-col gap-6 items-center">
+        <div class="flex flex-col gap-2 items-center">
             <span class="text-white">
                 Charachter
-                <hr class="w-[150px] border-0 h-[1px] bg-white/10">
+                <hr class="w-[150px] border-0 h-[1px] bg-white/10 mt-2">
             </span>
 
             <div class="flex items-center gap-10">
                 <button
                     :disabled="char >= 7531"
-                    @click="char++"
+                    @click="char += 10"
                     class="border border-primary text-primary hover:bg-primary hover:text-white duration-200 text-xl w-[30px] h-[30px] rounded">
                     +
                 </button>
@@ -17,16 +17,16 @@
                     {{ char }}
                 </span>
                 <button
-                    @click="char--"
+                    @click="char -= 10"
                     class="border border-primary text-primary hover:bg-primary hover:text-white duration-200 text-xl w-[30px] h-[30px] rounded">
                     -
                 </button>
             </div>
         </div>
-        <div class="flex flex-col gap-6 items-center">
+        <div class="flex flex-col gap-2 items-center">
             <span class="text-white">
-                Hello wolrd
-                <hr class="w-[150px] border-0 h-[1px] bg-white/10">
+                Paragraph
+                <hr class="w-[150px] border-0 h-[1px] bg-white/10 mt-2">
             </span>
             <div class="flex items-center gap-10">
                 <button
@@ -44,20 +44,6 @@
                 </button>
             </div>
         </div>
-        <div class="flex flex-col gap-6 items-center">
-            <span class="text-white">
-                Hello wolrd
-                <hr class="w-[150px] border-0 h-[1px] bg-white/10">
-            </span>
-            <div class="flex items-center gap-10">
-                <button
-                    class="border border-primary text-primary hover:bg-primary hover:text-white duration-200 text-xl w-[30px] h-[30px] rounded">+</button>
-                <span class="text-white">0</span>
-                <button
-                    class="border border-primary text-primary hover:bg-primary hover:text-white duration-200 text-xl w-[30px] h-[30px] rounded">-</button>
-            </div>
-        </div>
-        <button class="bg-primary text-white h-10 w-40 rounded border border-primary transform hover:bg-transparent duration-300">Create</button>
     </div>
 
 
@@ -65,7 +51,7 @@
         v-for="data in text" :key="data.id"
         class="w-full bg-primary/10 text-justify mt-10 p-4 rounded text-primary">
         <div v-for="i in paragraph" :key="i">
-            {{ data.english.length }}
+            {{ data.english.slice(0, char) }}
         </div>
     </div>
 </template>
