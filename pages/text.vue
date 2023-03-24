b5b2edd40b8fd4d7cb3246fa99a475065259b027
rayanramazan <rayanramazan09@gmail.com>
2023-03-24 19:51:47 +0300

    increment and decrement button

--- pages/text.vue
+++ pages/text.vue
@@ -1,17 +1,47 @@
 <template>
-    <div class="flex justify-center gap-8 sm:gap-20 flex-wrap mt-8 mb-4" style="z-index: 999;">
+    <div class="flex justify-center items-center gap-8 sm:gap-20 flex-wrap mt-8 mb-4 w-full" style="z-index: 999;">
         <div class="flex flex-col gap-6 items-center">
             <span class="text-white">
-                Hello wolrd
+                Charachter
                 <hr class="w-[150px] border-0 h-[1px] bg-white/10">
             </span>
 
             <div class="flex items-center gap-10">
                 <button
-                    class="border border-primary text-primary hover:bg-primary hover:text-white duration-200 text-xl w-[30px] h-[30px] rounded">+</button>
-                <span class="text-white">0</span>
+                    :disabled="char >= 7531"
+                    @click="char++"
+                    class="border border-primary text-primary hover:bg-primary hover:text-white duration-200 text-xl w-[30px] h-[30px] rounded">
+                    +
+                </button>
+                <span class="text-white">
+                    {{ char }}
+                </span>
                 <button
-                    class="border border-primary text-primary hover:bg-primary hover:text-white duration-200 text-xl w-[30px] h-[30px] rounded">-</button>
+                    @click="char--"
+                    class="border border-primary text-primary hover:bg-primary hover:text-white duration-200 text-xl w-[30px] h-[30px] rounded">
+                    -
+                </button>
+            </div>
+        </div>
+        <div class="flex flex-col gap-6 items-center">
+            <span class="text-white">
+                Hello wolrd
+                <hr class="w-[150px] border-0 h-[1px] bg-white/10">
+            </span>
+            <div class="flex items-center gap-10">
+                <button
+                    @click="paragraph++"
+                    class="border border-primary text-primary hover:bg-primary hover:text-white duration-200 text-xl w-[30px] h-[30px] rounded">
+                    +
+                </button>
+                <span class="text-white">
+                    {{ paragraph }}
+                </span>
+                <button
+                    @click="paragraph--"
+                    class="border border-primary text-primary hover:bg-primary hover:text-white duration-200 text-xl w-[30px] h-[30px] rounded">
+                    -
+                </button>
             </div>
         </div>
         <div class="flex flex-col gap-6 items-center">
@@ -27,42 +57,31 @@
                     class="border border-primary text-primary hover:bg-primary hover:text-white duration-200 text-xl w-[30px] h-[30px] rounded">-</button>
             </div>
         </div>
-        <div class="flex flex-col gap-6 items-center">
-            <span class="text-white">
-                Hello wolrd
-                <hr class="w-[150px] border-0 h-[1px] bg-white/10">
-            </span>
-            <div class="flex items-center gap-10">
-                <button
-                    class="border border-primary text-primary hover:bg-primary hover:text-white duration-200 text-xl w-[30px] h-[30px] rounded">+</button>
-                <span class="text-white">0</span>
-                <button
-                    class="border border-primary text-primary hover:bg-primary hover:text-white duration-200 text-xl w-[30px] h-[30px] rounded">-</button>
-            </div>
-        </div>
+        <button class="bg-primary text-white h-10 w-40 rounded border border-primary transform hover:bg-transparent duration-300">Create</button>
     </div>
 
-    <div class="w-full bg-primary/10 text-justify mt-10 p-4 rounded text-primary">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora officiis voluptas repudiandae rerum dolorem
-        velit sunt ipsa omnis consequatur eveniet voluptatibus quos aperiam maiores earum alias voluptate in obcaecati
-        consectetur, assumenda distinctio, quam perspiciatis. Veniam provident nesciunt quibusdam quam voluptate harum
-        itaque assumenda labore tempora consequuntur est animi maiores, odio ipsam, veritatis facere, exercitationem
-        ipsa minus iure. Vitae incidunt illo deleniti optio nulla magni a quisquam at laboriosam id! Ab deserunt nostrum
-        ad optio laudantium possimus adipisci quos laboriosam aspernatur quibusdam, animi soluta voluptatibus tempore
-        pariatur harum voluptatum temporibus cupiditate vitae commodi est culpa, fuga eaque magnam? Vitae libero, sint
-        tempora quae harum ipsa ullam, nam veritatis nemo enim in optio, voluptas numquam? Eveniet debitis sapiente
-        beatae sed quos, exercitationem quidem odio nam excepturi. Tenetur eum accusantium, exercitationem nam iure
-        suscipit ratione harum nostrum reiciendis esse sed voluptas ea doloribus at tempore quam ipsum tempora neque id.
-        Hic cumque facilis dolorem porro! Ratione corrupti molestiae assumenda, eveniet animi magnam quaerat doloremque
-        cum quod aliquid quasi illo quia officiis vitae, nisi iure a. Repellat saepe veniam unde! Molestias vitae ut
-        pariatur adipisci eligendi consequuntur nulla quisquam. A doloribus aliquam similique dolores possimus sit vitae
-        tempore assumenda reiciendis, nam itaque voluptatem! Aliquam ipsa mollitia hic consequuntur. Dolore possimus
-        dolorum et deleniti a dolores ad, hic, inventore, veritatis accusantium dolor. Tempora consequuntur aliquam in
-        autem placeat quibusdam minima quos, repellendus obcaecati est doloribus ad soluta corrupti recusandae unde
-        aliquid eveniet ratione molestiae dolor optio magnam culpa? Pariatur placeat iusto, repellat nostrum commodi
-        sapiente eos quibusdam ex quasi deleniti? Hic sed vero accusamus distinctio iure? Quisquam in hic saepe
-        asperiores aperiam nihil architecto numquam ut, impedit pariatur culpa atque! Excepturi enim velit accusantium
-        dignissimos, quis cumque ipsa iste voluptatibus rem culpa, quaerat at nostrum magni, ab provident qui sint
-        expedita. Aut facilis iusto ex?
+
+    <div 
+        v-for="data in text" :key="data.id"
+        class="w-full bg-primary/10 text-justify mt-10 p-4 rounded text-primary">
+        <div v-for="i in paragraph" :key="i">
+            {{ data.english.length }}
+        </div>
     </div>
 </template>
+
+<script setup lang="ts">
+const text = await useText()
+
+const char = ref(1000)
+const paragraph = ref(1)
+
+const incrementChar = () => {
+      char.value++
+}
+
+const decrementChar = () => {
+    char.value - 1
+}
+
+</script>
