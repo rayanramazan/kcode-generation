ac5112de965cba65349532985c32b468a26ba703
rayanramazan <rayanramazan09@gmail.com>
2023-04-01 02:21:12 +0300

    update

--- pages/gradients-colors.vue
+++ pages/gradients-colors.vue
@@ -1,16 +1,12 @@
 <template>
-
   <span class="mt-8 text-2xl text-white font-bold">
     Color Swatches
   </span>
   <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-x-2 gap-y-3 mt-3">
-  
-    <div 
-      v-for="gradient in gradients" :key="gradient.id"
-      class="flex rounded relative shadow-lg min-h-[160px] group overflow-hidden"
-      style="z-index: 1000;">
-      <div 
-        :style="`background: linear-gradient(270deg,${gradient.first_color} 0%,${gradient.second_color} 100%);`"
+
+    <div v-for="gradient in gradients" :key="gradient.id"
+      class="flex rounded relative shadow-lg min-h-[160px] group overflow-hidden" style="z-index: 1000;">
+      <div :style="`background: linear-gradient(270deg,${gradient.first_color} 0%,${gradient.second_color} 100%);`"
         class="w-full h-full group-hover:brightness-50 filter duration-500"></div>
       <div class="flex absolute justify-center w-full -bottom-16 group-hover:bottom-0 duration-300">
         <span class="bg-[#222] px-3 py-2 text-white text-sm">
@@ -18,12 +14,10 @@
         </span>
       </div>
     </div>
-  
+
   </div>
+</template>
   
-  
-  </template>
-  
-  <script setup lang="ts">
-  const gradients = await useGradients()
-  </script>
+<script setup lang="ts">
+const gradients = await useGradients()
+</script>
