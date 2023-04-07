be32ab982450570165eea3a50e069783c5e8ffa8
rayanramazan <rayanramazan09@gmail.com>
2023-04-08 02:53:55 +0300

    editor in buttons page

--- pages/buttons.vue
+++ pages/buttons.vue
@@ -1,16 +1,18 @@
 <template>
         <div class="flex flex-col" >
-            <!-- <span class="mt-8 text-2xl text-white font-bold">
-                {{ data.component.title }}
-            </span> -->
             <div class="grid md:grid-cols-2 lg:grid-cols-3 mt-3 gap-4 relative z-30">
-                <button v-for="data in buttons" :key="data.id" :class="data.component.className">Click Me</button>
+                <button 
+                @click="code = data.component.code"
+                v-for="data in buttons" :key="data.id" :class="data.component.className">Click Me</button>
             </div>
-
         </div>
+
+        <CodeEditor :code="code" lang="css" />
 
 </template>
     
 <script setup lang="ts">
 const buttons = await useButtons()
+
+const code = ref('')
 </script>
