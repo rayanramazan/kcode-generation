11b1ca76977a3718d022442c05e28b0a68a28565
rayanramazan <rayanramazan09@gmail.com>
2023-04-08 05:23:38 +0300

    Update minifier.vue

--- pages/minifier.vue
+++ pages/minifier.vue
@@ -8,8 +8,8 @@
    <div class="w-full flex flex-col mt-5">
     <textarea v-model="minifier" rows="20" class="outline-none bg-primary/30 rounded text-white p-4"></textarea>
     <div class="flex justify-between items-center mt-4">
-        <button @click="x" class="bg-primary p-2 rounded text-white hover:-translate-y-1 transform duration-300">Remove Spaces and Line Breaks</button>
-        <button class="bg-white text-primary p-2 rounded hover:-translate-y-1 transform duration-300">Clear</button>
+        <button class="bg-primary p-2 rounded text-white hover:-translate-y-1 transform duration-300">Remove Spaces and Line Breaks</button>
+        <button @click="minifier = ''" class="bg-white text-primary p-2 rounded hover:-translate-y-1 transform duration-300">Clear</button>
     </div>
   </div>
 
@@ -17,9 +17,12 @@
         Minified CSS Output
     </span>
    <div class="w-full flex flex-col mt-5">
-    <textarea rows="20" class="outline-none bg-primary/30 rounded text-white p-4" disabled v-model="minifier"></textarea>
+    <textarea rows="20" class="outline-none bg-primary/30 rounded text-white p-4" disabled v-model="minifier" id="copy-input"></textarea>
     <div class="flex justify-start items-center mt-4">
-        <button class="bg-primary p-2 rounded text-white hover:-translate-y-1 transform duration-300">Copy to Clipboard</button>
+        <button 
+            @click="copyText"
+            class="bg-primary p-2 rounded text-white hover:-translate-y-1 transform duration-300">Copy to Clipboard</button>
+
     </div>
   </div>
 
@@ -28,9 +31,28 @@
 
 <script setup lang="ts">
 const minifier = ref('')
+const textToCopy = ref(null)
+const response = ref(false)
 
-const x = watch(minifier, (newValue) => {
+watch(minifier, (newValue) => {
     minifier.value = newValue.replace(/\s+/g, '')
+})
+
+
+const copyText = () => {
+  if (textToCopy.value) {
+    const text = textToCopy.value.innerText
+    navigator.clipboard.writeText(text)
+    response.value = true
+  }
+}
+
+watch(response, (newValue) => {
+  if (newValue) {
+    setTimeout(() => {
+      response.value = false
+    }, 3000)
+  }
 })
 
 </script>
