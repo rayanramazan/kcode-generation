7ccedd2842e071711260a4df297000885e798163
rayanramazan <rayanramazan09@gmail.com>
2023-04-08 04:50:01 +0300

    complete buttons components

--- pages/buttons.vue
+++ pages/buttons.vue
@@ -1,5 +1,11 @@
 <template>
-        <div class="flex flex-col" >
+    <span class="mt-8 mb-4 text-2xl text-white font-bold">
+        Transitions Delay
+    </span>
+    <p class="text-white/80">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat eum optio ipsam error cumque dolorem ex molestias aspernatur facere laudantium.
+    </p>
+        <div class="flex flex-col mt-4 mb-10">
             <div class="grid md:grid-cols-2 lg:grid-cols-3 mt-3 gap-4 relative z-30">
                 <button 
                 @click="code = data.component.code"
@@ -7,7 +13,8 @@
             </div>
         </div>
 
-        <CodeEditor :code="code" lang="css" />
+
+        <CodeEditor :code="code" lang="css" v-show="code == null"/>
 
 </template>
     
