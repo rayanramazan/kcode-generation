b822a7e69efd3d416a1beb10d694c781c5a48792
rayanramazan <rayanramazan09@gmail.com>
2023-04-08 05:40:51 +0300

    add meta page

---
+++ pages/meta.vue
@@ -0,0 +1,12 @@
+<template>
+    <span class="mt-8 text-2xl text-white font-bold">
+        Meta Tags Generator
+    </span>
+    <p class="text-white/80">
+        Generate your web page’s most helpful meta tags to improve SEO and search engine experience.
+    </p>
+
+    <div class="grid md:grid-cols-2 lg:grid-cols-3 mt-3 gap-4 relative z-30">
+        
+    </div>
+</template>
