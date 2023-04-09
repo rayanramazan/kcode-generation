47a3d25bac271345cbc170c064c518fdfccf3c13
rayanramazan <rayanramazan09@gmail.com>
2023-04-09 10:41:44 +0300

    redesign resource page ( card )

--- pages/resource.vue
+++ pages/resource.vue
@@ -6,33 +6,37 @@
        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat eum optio ipsam error cumque dolorem ex molestias aspernatur facere laudantium.
    </p>
 
-   <div class="grid md:grid-cols-2 lg:grid-cols-3 mt-3 gap-4 relative z-30">
+   <div class="grid md:grid-cols-2 lg:grid-cols-3 mt-6 gap-4 relative z-30">
 
-       <div class="bg-[#222] flex flex-col justify-between p-4 w-full h-90 border border-primary/40 rounded-lg">
+       <div 
+        v-for="data in resources" :key="data.id"
+        class="bg-[#222] flex flex-col justify-between p-4 w-full border border-primary/40 rounded-lg">
            <div>
-               <img 
-                   class="w-20 h-20"
-                   src="https://upload.wikimedia.org/wikipedia/commons/a/ab/Android_O_Preview_Logo.png" alt="" srcset="">
-               <h1 class="text-xl text-white mt-6">
-                   Color Hunt
+               <h1 class="text-2xl text-white">
+                   {{ data.title }}
                </h1>
                <p class="text-white/70">
-                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur doloremque natus optio, cumque numquam perferendis alias.
+                   {{ data.description }}
                </p>
            </div>
+           <hr class="my-4 border-0 bg-white/20 h-[1px] w-full">
            <div class="flex justify-between items-center">
                <button class="flex items-center gap-1 text-white">
                    <span>12</span> 
                    <Icon name="ic:baseline-star-border" class="w-6 h-6"/>
                </button>
 
-               <a href="" class="flex gap-1 items-center group">
+               <NuxtLink :to="data.link" class="flex gap-1 items-center group">
                    <span class="text-white transform group-hover:mr-2 duration-300 group-hover:text-primary">Website</span>
                    <Icon name="ic:baseline-keyboard-arrow-right" class="w-6 h-6 text-primary"/>
-               </a>
+               </NuxtLink>
            </div>
        </div>
 
 
    </div>
 </template>
+
+<script setup lang="ts">
+const resources = await useResources()
+</script>
