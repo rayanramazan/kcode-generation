77f21b57e8fb47da6e042dbcd7188295ade2ec1f
rayanramazan <rayanramazan09@gmail.com>
2023-04-15 16:50:45 +0300

    change name spinner to loader-generator

--- pages/loader-generator.vue
+++ pages/loader-generator.vue
@@ -3,7 +3,7 @@
   <div 
     class="grid md:grid-cols-3 lg:grid-cols-4 gap-x-2 gap-y-3 mt-3">
     <div     
-    v-for="spinner in data.spinner" 
+    v-for="spinner in data['loader-generator']" 
     :key="spinner.id"
     v-html="spinner.html"
     class="bg-[#222] h-40 flex justify-center items-center z-30 rounded border border-white/10 duration-300 hover:border-primary">
