20d395f6ca30119451fa7a4d2c4931356815bb30
rayanramazan <rayanramazan09@gmail.com>
2023-04-15 17:01:33 +0300

    add code editor to loader page

--- pages/loader-generator.vue
+++ pages/loader-generator.vue
@@ -1,17 +1,26 @@
 <template>
-   
   <div 
-    class="grid md:grid-cols-3 lg:grid-cols-4 gap-x-2 gap-y-3 mt-3">
-    <div     
-    v-for="spinner in data['loader-generator']" 
-    :key="spinner.id"
-    v-html="spinner.html"
-    class="bg-[#222] h-40 flex justify-center items-center z-30 rounded border border-white/10 duration-300 hover:border-primary">
-  </div>
+    class="grid lg:grid-cols-2 gap-x-2 gap-y-3 mt-3">
+
+    <div 
+      class="grid md:grid-cols-2 lg:grid-cols-3 gap-x-2 gap-y-3">
+      <div     
+      v-for="spinner in data['loader-generator']" 
+      :key="spinner.id"
+      v-html="spinner.html"
+      @click="code = spinner.css"
+      class="bg-[#222] h-40 flex justify-center items-center z-30 rounded border border-white/10 duration-300 hover:border-primary">
+      </div>
+    </div>
+
+    <CodeEditor :code="code" />
+
   </div>
     
 </template>
 
 <script setup lang="ts">
 const data = await useComponents()
+
+const code = ref('')
 </script>
