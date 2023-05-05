8b6b60ee752b2c4eb481bb5feb644821748b6a31
rayanramazan <rayanramazan09@gmail.com>
2023-05-05 23:09:56 +0300

    Update buttons.vue

--- pages/buttons.vue
+++ pages/buttons.vue
@@ -3,7 +3,7 @@
         {{ $t('buttons-generator') }}
     </span>
     <p class="text-white/80">
-        {{ $t('d-buttons-generator') }}
+        {{ $t('d-button-generation') }}
     </p>
         <div class="flex flex-col mt-4 mb-10">
             <div class="grid md:grid-cols-2 lg:grid-cols-3 mt-3 gap-4 relative z-30">
