d85e2423599a96627d975aea5c1841b944a7aa96
rayanramazan <rayanramazan09@gmail.com>
2023-05-06 01:19:22 +0300

    button title and description

--- pages/index.vue
+++ pages/index.vue
@@ -60,10 +60,10 @@
 
 
     <span class="mt-4 mb-4 text-2xl text-white font-bold">
-        Transitions Delay
+        {{ $t('buttons-generator') }}
     </span>
     <p class="text-white/80">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat eum optio ipsam error cumque dolorem ex molestias aspernatur facere laudantium.
+        {{ $t('d-button') }}
     </p>
     <div class="flex flex-col mt-4 mb-10 w-full">
         <div class="grid md:grid-cols-2 lg:grid-cols-3 mt-3 gap-4 relative z-30">
