58de0c5e50a3de99ff099f9f84e74707244e2411
rayanramazan <rayanramazan09@gmail.com>
2023-05-08 01:11:05 +0300

    Update gradients-colors.vue

--- pages/gradients-colors.vue
+++ pages/gradients-colors.vue
@@ -9,10 +9,16 @@
       <div :style="`background: linear-gradient(270deg,${gradient.first_color} 0%,${gradient.second_color} 100%);`"
         class="w-full h-full group-hover:brightness-50 filter duration-500"></div>
       <div class="flex absolute justify-center w-full -bottom-16 group-hover:bottom-0 duration-300">
-        <span class="bg-[#222] px-3 py-2 text-white text-sm">
+        <span class="bg-gradient-to-r from-[#222] to-[#111] px-3 py-2 text-white text-center text-sm w-full">
           background: linear-gradient(270deg,{{ gradient.first_color }}0%,{{ gradient.second_color }} 100%);
         </span>
       </div>
+      <button class="flex absolute justify-end pr-2  w-full -top-16 group-hover:top-2 duration-300 ">
+        <span class="bg-gradient-to-r from-[#222] to-[#111] px-3 py-2 text-white text-center text-sm rounded-lg flex items-center gap-1 cursor-pointer">
+          <span>Copy</span>
+          <Icon name="solar:copy-broken" class="w-5 h-5" />
+        </span>
+      </button>
     </div>
 
   </div>
