0e430608bb71798795b8514c8b1751467fae4486
Rayan Ramazan <rayanramazan09@gmail.com>
2023-07-15 15:20:40 +0300

    update

--- layouts/header.vue
+++ layouts/header.vue
@@ -2,7 +2,7 @@
   <navbar />
   <div class="w-full gap-4 items-start flex flex-row-reverse">
     <div class="w-full flex flex-col">
-      <div class="py-4 flex justify-between lg:justify-end items-center px-4 bg-white/5 rounded-md border-1 border-white/10">
+      <div class="py-4 flex justify-between lg:justify-end items-center px-4 rounded-md">
 
         <div class="flex items-center gap-4">
           <button class="block lg:hidden" @click="isOpen = !isOpen">
@@ -10,12 +10,12 @@
           </button>
         <div 
         dir="ltr"
-        class="flex lg:hidden justify-center items-center gap-1 text-2xl lg:text-3xl font-bold">
-          <span class="text-primary">
-            K
+        class="flex lg:hidden justify-center items-end gap-1 text-2xl lg:text-3xl font-bold text-white">
+          <span>
+            KCODE
           </span>
-          <span class="text-white">
-            TOOLS
+          <span class="text-white font-normal text-sm mb-[1.1px]">
+            Generation
           </span>
           </div>
         </div>
@@ -23,35 +23,6 @@
 
         <ButtonLang class="btn_lang" />
 
-        <div class="flex head_social items-center gap-3 text-white">
-          <ButtonLang />
-          <hr class="mx-5 w-[0.5px] border-0 bg-white/10 h-8">
-          <NuxtLink to="">
-            <Icon
-                class="w-6 h-6 transform hover:-translate-y-1 cursor-pointer duration-300"
-                name="ion:logo-instagram" />
-          </NuxtLink>
-          <NuxtLink to="">
-            <Icon
-                class="w-6 h-6 transform hover:-translate-y-1 cursor-pointer duration-300"
-                name="ion:social-facebook" />
-          </NuxtLink>
-          <NuxtLink to="">
-            <Icon
-                class="w-6 h-6 transform hover:-translate-y-1 cursor-pointer duration-300"
-                name="ion:logo-github" />
-          </NuxtLink>
-          <NuxtLink to="">
-            <Icon
-                class="w-6 h-6 transform hover:-translate-y-1 cursor-pointer duration-300"
-                name="ion:logo-twitter" />
-          </NuxtLink>
-          <NuxtLink to="">
-            <Icon
-                class="w-6 h-6 transform hover:-translate-y-1 cursor-pointer duration-300"
-                name="ion:logo-linkedin" />
-          </NuxtLink>
-        </div>
 
       </div>
       <slot />
--- layouts/navbar.vue
+++ layouts/navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full flex justify-between items-center">
+  <div class="w-full md:flex none justify-between items-center">
     <div class="flex items-end gap-2">
       <h1 class="text-4xl font-semibold text-white ">KCODE</h1>
       <span class="text-2xl text-white">Generation</span>
@@ -7,7 +7,6 @@
 
     <div class="flex items-center gap-6">
       <ButtonLang />
-      <!-- <Icon name="iconoir:translate" class="w-[24px] h-[24px] text-white"/> -->
       <hr class="h-8 bg-white w-[1px] ">
       <Icon name="iconoir:github" class="w-[24px] h-[24px] text-white"/>
     </div>
