eb93d3525301e3a601b4aee25a7b2675f8f45d17
Rayan Ramazan <rayanramazan09@gmail.com>
2023-07-22 00:35:26 +0300

    Update text.vue

--- pages/text.vue
+++ pages/text.vue
@@ -1,41 +1,39 @@
 <template>
     <div class="flex justify-center items-center gap-8 sm:gap-20 flex-wrap mt-26 lg:p-0 p-4 mb-4 w-full" style="z-index: 999;">
-        <div class="flex flex-col gap-2 items-center">
+        <div class="flex flex-col gap-2 items-start">
             <span class="text-white">
                 {{ $t('charachter') }}
-                <hr class="w-[150px] border-0 h-[1px] bg-white/10 mt-2">
             </span>
 
             <div class="flex items-center gap-10">
                 <button
                     :disabled="char >= 7531"
                     @click="char += 10"
-                    class="border border-primary text-primary hover:bg-primary hover:text-white duration-200 text-xl w-[30px] h-[30px] rounded">
+                    class="border border-secondary/50 text-secondary hover:bg-secondary hover:text-primary duration-200 text-xl w-[30px] h-[30px] rounded">
                     +
                 </button>
                 <input type="number" v-model="char" class="text-white text-center bg-transparent border-0 outline-none ring-0" />
                 <button
                     @click="char -= 10"
-                    class="border border-primary text-primary hover:bg-primary hover:text-white duration-200 text-xl w-[30px] h-[30px] rounded">
+                    class="border border-secondary/50 text-secondary hover:bg-secondary hover:text-primary duration-200 text-xl w-[30px] h-[30px] rounded">
                     -
                 </button>
             </div>
         </div>
-        <div class="flex flex-col gap-2 items-center">
+        <div class="flex flex-col gap-2 items-start">
             <span class="text-white">
                 {{ $t('paragraph') }}
-                <hr class="w-[150px] border-0 h-[1px] bg-white/10 mt-2">
             </span>
             <div class="flex items-center gap-10">
                 <button
                     @click="paragraph++"
-                    class="border border-primary text-primary hover:bg-primary hover:text-white duration-200 text-xl w-[30px] h-[30px] rounded">
+                    class="border border-secondary/50 text-secondary hover:bg-secondary hover:text-primary duration-200 text-xl w-[30px] h-[30px] rounded">
                     +
                 </button>
                 <input type="number" v-model="paragraph" class="text-white text-center bg-transparent border-0 outline-none ring-0" />
                 <button
                     @click="paragraph--"
-                    class="border border-primary text-primary hover:bg-primary hover:text-white duration-200 text-xl w-[30px] h-[30px] rounded">
+                    class="border border-secondary/50 text-secondary hover:bg-secondary hover:text-primary duration-200 text-xl w-[30px] h-[30px] rounded">
                     -
                 </button>
             </div>
@@ -45,7 +43,7 @@
 
     <div 
         v-for="data in text" :key="data.id"
-        class="w-full bg-primary/10 text-justify mt-10 p-4 rounded text-primary">
+        class="w-full bg-secondary/10 text-justify mt-10 p-4 lg:mr-4 rounded text-secondary">
         <div v-for="i in paragraph" :key="i">
             {{ data.english.slice(0, char) }}
         </div>
