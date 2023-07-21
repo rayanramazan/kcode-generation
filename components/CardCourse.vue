397adc5e947ae5c0090e5e539f8c8546e8e22e66
Rayan Ramazan <rayanramazan09@gmail.com>
2023-07-21 14:22:05 +0300

    update

--- components/CardCourse.vue
+++ components/CardCourse.vue
@@ -1,64 +1,64 @@
 <template>
-    <div class="grid lg:grid-cols-3 md:grid-cols-2 gap-4">
+    <div class="grid lg:grid-cols-3 md:grid-cols-2 gap-4 lg:mr-4">
         <div class="h-20 card-course flex justify-between items-center px-4">
             <div class="flex flex-col">
-                <div class="flex text-2xl text-white items-center gap-2">
+                <div class="flex text-xl lg:text-2xl text-white items-center gap-2">
                     <span>Course</span>
                     <span class="font-bold">HTML</span>
                 </div>
-                <span class="text-white text-base">coming soon</span>
+                <span class="text-white text-sm lg:text-base">coming soon</span>
             </div>
-            <Icon name="uil:arrow-growth" class="w-9 h-9 text-white"/>
+            <Icon name="uil:arrow-growth" class="lg:w-9 w-7 lg:h-9 h-7 text-white"/>
         </div>
         <div class="h-20 card-course flex justify-between items-center px-4">
             <div class="flex flex-col">
-                <div class="flex text-2xl text-white items-center gap-2">
+                <div class="flex text-xl lg:text-2xl text-white items-center gap-2">
                     <span>Course</span>
                     <span class="font-bold">CSS</span>
                 </div>
-                <span class="text-white text-base">coming soon</span>
+                <span class="text-white text-sm lg:text-base">coming soon</span>
             </div>
-            <Icon name="uil:arrow-growth" class="w-9 h-9 text-white"/>
+            <Icon name="uil:arrow-growth" class="lg:w-9 w-7 lg:h-9 h-7 text-white"/>
         </div>
         <div class="h-20 card-course flex justify-between items-center px-4">
             <div class="flex flex-col">
-                <div class="flex text-2xl text-white items-center gap-2">
+                <div class="flex text-xl lg:text-2xl text-white items-center gap-2">
                     <span>Course</span>
                     <span class="font-bold">Vue.JS</span>
                 </div>
-                <span class="text-white text-base">coming soon</span>
+                <span class="text-white text-sm lg:text-base">coming soon</span>
             </div>
-            <Icon name="uil:arrow-growth" class="w-9 h-9 text-white"/>
+            <Icon name="uil:arrow-growth" class="lg:w-9 w-7 lg:h-9 h-7 text-white"/>
         </div>
         <div class="h-20 card-course flex justify-between items-center px-4">
             <div class="flex flex-col">
-                <div class="flex text-2xl text-white items-center gap-2">
+                <div class="flex text-xl lg:text-2xl text-white items-center gap-2">
                     <span>Course</span>
                     <span class="font-bold">Bootstrap</span>
                 </div>
-                <span class="text-white text-base">coming soon</span>
+                <span class="text-white text-sm lg:text-base">coming soon</span>
             </div>
-            <Icon name="uil:arrow-growth" class="w-9 h-9 text-white"/>
+            <Icon name="uil:arrow-growth" class="lg:w-9 w-7 lg:h-9 h-7 text-white"/>
         </div>
         <div class="h-20 card-course flex justify-between items-center px-4">
             <div class="flex flex-col">
-                <div class="flex text-2xl text-white items-center gap-2">
+                <div class="flex text-xl lg:text-2xl text-white items-center gap-2">
                     <span>Course</span>
                     <span class="font-bold">Tailwind CSS</span>
                 </div>
-                <span class="text-white text-base">coming soon</span>
+                <span class="text-white text-sm lg:text-base">coming soon</span>
             </div>
-            <Icon name="uil:arrow-growth" class="w-9 h-9 text-white"/>
+            <Icon name="uil:arrow-growth" class="lg:w-9 w-7 lg:h-9 h-7 text-white"/>
         </div>
         <div class="h-20 card-course flex justify-between items-center px-4">
             <div class="flex flex-col">
-                <div class="flex text-2xl text-white items-center gap-2">
+                <div class="flex text-xl lg:text-2xl text-white items-center gap-2">
                     <span>Course</span>
                     <span class="font-bold">Nuxt.JS</span>
                 </div>
-                <span class="text-white text-base">coming soon</span>
+                <span class="text-white text-sm lg:text-base">coming soon</span>
             </div>
-            <Icon name="uil:arrow-growth" class="w-9 h-9 text-white"/>
+            <Icon name="uil:arrow-growth" class="lg:w-9 w-7 lg:h-9 h-7 text-white"/>
         </div>
     </div>
 </template>
