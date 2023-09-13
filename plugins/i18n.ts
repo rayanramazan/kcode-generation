64f2d310ca80492b833ef6946b0a4b8338ca2930
Rayan Ramazan <88462162+rayanramazan@users.noreply.github.com>
2023-09-13 14:35:55 +0300

    Update i18n.ts

--- plugins/i18n.ts
+++ plugins/i18n.ts
@@ -3,6 +3,7 @@ export default defineNuxtPlugin(nuxtApp => {
     nuxtApp.$i18n.onBeforeLanguageSwitch = (oldLocale: any, newLocale: any, isInitialSetup: any, nuxtApp: any) => {
         console.log('onBeforeLanguageSwitch', oldLocale, newLocale, isInitialSetup)
     }
+    
     // onLanguageSwitched called right after a new locale has been set
     nuxtApp.$i18n.onLanguageSwitched = (oldLocale: any, newLocale: any) => {
         console.log('onLanguageSwitched', oldLocale, newLocale)
